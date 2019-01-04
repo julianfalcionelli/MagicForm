c2a7396ca6e4d1bf3725951e316b0b2ec37ec230
Julian Falcionelli <julianfalcionelli@gmail.com>
2019-01-04 12:42:13 +0100

    Android X Migration.
    Target SDK LVL 28.
    Use Android X Libraries.
    Updated Gradle version.

--- app/src/main/java/julianfalcionelli/magicform/helper/ValidationHelper.java
+++ app/src/main/java/julianfalcionelli/magicform/helper/ValidationHelper.java
@@ -1,12 +1,14 @@
 package julianfalcionelli.magicform.helper;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputLayout;
 import android.view.View;
 import android.view.ViewParent;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class ValidationHelper
 {
